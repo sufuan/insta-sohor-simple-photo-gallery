1b9f54c1d8271f4c0b5278258eac8fcf77df5764
sufuan <abu49539@gmail.com>
2022-03-11 12:30:54 +0600

    q&a added

--- js/app.js
+++ js/app.js
@@ -37,16 +37,21 @@ const switchTab = (id) => {
         document.getElementById("posts").style.display = "grid";
         document.getElementById("liked").style.display = "none";
         document.getElementById("reported").style.display = "none";
+        document.getElementById("faq").style.display = "block";
+
     } else if (id === "liked") {
         document.getElementById("liked").style.display = "block";
         document.getElementById("posts").style.display = "none";
         document.getElementById("reported").style.display = "none";
+        document.getElementById("faq").style.display = "none";
 
         displayLikedPosts();
     } else {
         document.getElementById("reported").style.display = "block";
         document.getElementById("posts").style.display = "none";
         document.getElementById("liked").style.display = "none";
+        document.getElementById("faq").style.display = "none";
+
 
         displayReportedPosts();
     }
